fe16547ee68f2cb30fb0aca54728661192c69056
Melik Soufiane <101507986+S-Melik@users.noreply.github.com>
2024-06-06 02:11:13 +0200

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -6,7 +6,7 @@ host = 'localhost'  # or '127.0.0.1'
 user = 'root'
 password = '1381'  # Replace with your actual password
 database = 'health_insurance'
-query = 'SELECT * FROM data_table2024'  # Adjust your query as needed
+query = 'SELECT * FROM data_table2023'  # Adjust your query as needed
 
 # Connect to the database
 connection = pymysql.connect(
@@ -20,7 +20,7 @@ connection = pymysql.connect(
 df = pd.read_sql(query, connection)
 
 # Save the DataFrame to a CSV file
-csv_file_path = 'extracted_data2024.csv'
+csv_file_path = 'extracted_data2023.csv'
 df.to_csv(csv_file_path, index=False)
 
 # Close the database connection
@@ -31,7 +31,7 @@ print(f"Data extracted and saved to {csv_file_path}")
 # ----------------------------------------------------------------------------------------------------------------------------------------
 
 # Load data from the extracted CSV file
-df = pd.read_csv('extracted_data2024.csv')
+df = pd.read_csv('extracted_data2023.csv')
 
 # Step 1: Remove Blank Rows
 df = df[~((df['Declaration'] == '') & (df['Date reception'] == '') & 
@@ -56,6 +56,6 @@ df['Frais engages'] = pd.to_numeric(df['Frais engages'], errors='coerce').fillna
 df['Code maladie'] = pd.to_numeric(df['Code maladie'], errors='coerce').fillna(0).astype(int)
 
 # Save the cleaned data to a new CSV file
-df.to_csv('extracted_data2024_cleaned.csv', index=False)
+df.to_csv('extracted_data2023_cleaned.csv', index=False)
 
 print("Data cleaning and standardization complete. Cleaned data saved to 'extracted_data2024_cleaned.csv'")
